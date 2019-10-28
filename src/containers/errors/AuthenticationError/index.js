b2cf15af1c7bf8814e0ec7131e3ea56349f35766
AlaaTaima <alaa.ktaima@gmail.com>
2019-10-28 23:48:29 +0200

    create athentication error page Relates #15

---
+++ src/containers/errors/AuthenticationError/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Button } from 'antd';
+import { ReactComponent as Illustration } from './vector.svg';
+import './style.css';
+
+const UnAthenticated = () => {
+  return (
+    <div className="error">
+      <Illustration />
+      <p className="error__message">
+        Sorry but you are not authorized to see the content of this page
+      </p>
+      <Button className="error__button" type="primary">
+        Go Home
+      </Button>
+    </div>
+  );
+};
+
+export default UnAthenticated;
