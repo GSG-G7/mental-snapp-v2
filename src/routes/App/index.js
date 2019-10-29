d4de560b41ecb7a7fb64c1b929b472ee8cdfa6d7
alaa-yasin <alaayasin.ps@gmail.com>
2019-10-29 19:08:43 +0200

    remove ServerError page from athe app Relates #13

--- src/routes/App/index.js
+++ src/routes/App/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import 'antd/dist/antd.css';
-import ServerError from '../../containers/errors/ServerError';
 import './style.css';
 
 const App = () => {
   return (
     <div className="App">
       <h1>Hello from App</h1>
-      <ServerError />
     </div>
   );
 };
