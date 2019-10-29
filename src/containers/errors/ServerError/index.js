825a38235ca5851f204129b85c790ba34c5a0a99
alaa-yasin <alaayasin.ps@gmail.com>
2019-10-29 15:01:10 +0200

    use antd button Relates #13

--- src/containers/errors/ServerError/index.js
+++ src/containers/errors/ServerError/index.js
@@ -5,13 +5,15 @@ import './style.css';
 
 const ServerError = () => {
   return (
-    <div className="servererror">
-      <Image className="servererror__img" />
-      <p className="servererror__msg">
+    <div className="server-error">
+      <Image className="server-error__img" />
+      <p className="server-error__msg">
         Oh sorry about this problem our development team is working to on it
         now.
       </p>
-      <Button className="servererror__btn">Go Home</Button>
+      <Button className="server-error__btn" type="primary">
+        Go Home
+      </Button>
     </div>
   );
 };
--- src/routes/App/index.js
+++ src/routes/App/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import 'antd/dist/antd.css';
+import ServerError from '../../containers/errors/ServerError';
 import './style.css';
 
 const App = () => {
   return (
     <div className="App">
       <h1>Hello from App</h1>
+      <ServerError />
     </div>
   );
 };
