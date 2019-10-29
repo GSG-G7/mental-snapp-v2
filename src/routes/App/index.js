e4974ce9d7be7f09e8f34c811c0fb6e78285e695
AlaaTaima <alaa.ktaima@gmail.com>
2019-10-29 11:21:39 +0200

    return app/index.js to it's default Relates #28

--- src/routes/App/index.js
+++ src/routes/App/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import './style.css';
-import LandingPage from '../../containers/Landing/index';
 
 const App = () => {
-  return (
-    <div className="App">
-      <LandingPage />
-    </div>
-  );
+  return <div className="App" />;
 };
 
 export default App;
