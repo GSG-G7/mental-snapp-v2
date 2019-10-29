7bf68ed9b1bb9621663746bf20e7319071ebb89b
AlaaTaima <alaa.ktaima@gmail.com>
2019-10-29 13:50:32 +0200

    edit back button width Relates #56

--- src/routes/App/index.js
+++ src/routes/App/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import './style.css';
+import Button from '../../components/BackButton/index';
 
 const App = () => {
   return (
     <div className="App">
       <h1>Hello from App</h1>
+      <Button />
     </div>
   );
 };
