2262578c223a5d92d1fea11be71ee3baa271a116
fares98 <faresalhello21@gmail.com>
2019-10-29 15:47:21 +0200

    page underconstruction

--- src/containers/UnderConstruction/index.js
+++ src/containers/UnderConstruction/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
-
+import BackButton from '../../components/BackButton';
 import { ReactComponent as ConstructionImg } from '../assets/images/underConstruction.svg';
 import './style.css';
 
 const UnderConstruction = () => {
   return (
     <div className="constraction">
-      <ConstructionImg />
+      <div style={{ textAlign: 'left' }}>
+        <BackButton />
+      </div>
+      <div style={{ paddingTop: '20vh' }}>
+        <ConstructionImg />
+      </div>
       <h2 className="constraction__heading">
         <p className="constraction__heading__p">page</p>
         under construction
--- src/routes/App/index.js
+++ src/routes/App/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import './style.css';
-import Button from '../../components/BackButton/index';
 
 const App = () => {
   return (
     <div className="App">
       <h1>Hello from App</h1>
-      <Button />
     </div>
   );
 };
