8e1492134d74840a04d9019e97ee85a6dcadfe4d
AlaaTaima <alaa.ktaima@gmail.com>
2019-10-30 08:56:29 +0200

    edit style Relates #28

--- src/containers/Landing/index.js
+++ src/containers/Landing/index.js
@@ -7,20 +7,18 @@ const LandingPage = () => {
   return (
     <div>
       <Illustration className="Illustration" />
-      <div className="landingPAge">
-        <p className="landingPage__title">
-          Practice the art of good mental health
-        </p>
-        <p className="landingPage__content">
+      <div className="landing">
+        <p className="landing__title">Practice the art of good mental health</p>
+        <p className="landing__content">
           Tell your story and positively guide your thoughts
         </p>
-        <Button type="primary" className="landingPage__button">
+        <Button type="primary" className="landing__button">
           Sign In
         </Button>
-        <p className="landingPage__signUpLink">Create a new account</p>
-        <p className="landingPage__aboutLink">
+        <p className="landing__link">Create a new account</p>
+        <p className="landing__aboutLink">
           Read more about
-          <span className="landingPage__logo">Mental Snapp</span>
+          <span className="landing__logo">Mental Snapp</span>
         </p>
       </div>
     </div>
