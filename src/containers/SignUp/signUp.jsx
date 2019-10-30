8ab04cb55078d4d0a40c174176cfe1e450e62961
fares98 <faresalhello21@gmail.com>
2019-10-30 10:43:02 +0200

    statr work on the sign up

---
+++ src/containers/SignUp/signUp.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Input } from 'antd';
+
+const signUp = () => {
+  return (
+    <div>
+      <form>
+        <Input size="large" />
+      </form>
+    </div>
+  );
+};
+
+export default signUp;
