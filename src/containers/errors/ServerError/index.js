14117dfbd43ff1d2cb5019c5e855c8d3d5374717
alaa-yasin <alaayasin.ps@gmail.com>
2019-10-30 10:26:05 +0200

    fix accessibility and add media query Relates #13

--- src/containers/errors/ServerError/index.js
+++ src/containers/errors/ServerError/index.js
@@ -5,16 +5,18 @@ import './style.css';
 
 const ServerError = () => {
   return (
-    <div className="server-error">
-      <Image className="server-error__img" />
-      <p className="server-error__msg">
-        Oh sorry about this problem our development team is working to on it
-        now.
-      </p>
-      <Button className="server-error__btn" type="primary">
-        Go Home
-      </Button>
-    </div>
+    <main aria-label="main content of the page">
+      <div className="server-error">
+        <Image className="server-error__img" alt="server error image" />
+        <p className="server-error__msg">
+          Oh sorry about this problem our development team is working to on it
+          now.
+        </p>
+        <Button className="server-error__btn" type="primary">
+          Go Home
+        </Button>
+      </div>
+    </main>
   );
 };
 
