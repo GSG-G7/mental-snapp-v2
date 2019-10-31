d232d62d0121879cb986aff5a8c3ef886af12eef
alaa-yasin <alaayasin.ps@gmail.com>
2019-10-31 04:12:59 +0200

    add the routes and a new class name Relates #57

--- src/components/navigationBar/index.jsx
+++ src/components/navigationBar/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { ReactComponent as HomeIcon } from '../assets/icons/homeIcon.svg';
 import { ReactComponent as Calendar } from '../assets/icons/calendar.svg';
 import { ReactComponent as AddNew } from '../assets/icons/addNew.svg';
 import { ReactComponent as FeedsFilter } from '../assets/icons/feedsFilter.svg';
 import { ReactComponent as Settings } from '../assets/icons/settings.svg';
+import * as ROUTES from '../../constants/routes';
 import './navigation.css';
 
 const NavBar = () => {
@@ -12,29 +13,29 @@ const NavBar = () => {
     <div className="navbar">
       <nav>
         <i>
-          <Link to="/home">
+          <NavLink to={ROUTES.HOME} activeClassName="clicked">
             <HomeIcon className="navbar__home-icon" />
-          </Link>
+          </NavLink>
         </i>
         <i>
-          <Link to="/home">
+          <NavLink to={ROUTES.HEAT_MAP} activeClassName="clicked">
             <Calendar className="navbar__calendar" />
-          </Link>
+          </NavLink>
         </i>
         <i>
-          <Link to="/home">
+          <NavLink to={ROUTES.QUESTION} activeClassName="clicked">
             <AddNew className="navbar__add-icon" />
-          </Link>
+          </NavLink>
         </i>
         <i>
-          <Link to="/home">
+          <NavLink to={ROUTES.FEED} activeClassName="clicked">
             <FeedsFilter className="navbar__feeds-filter" />
-          </Link>
+          </NavLink>
         </i>
         <i>
-          <Link to="/home">
+          <NavLink to={ROUTES.ACCOUNT_SETTINGS} activeClassName="clicked">
             <Settings className="navbar__settings" />
-          </Link>
+          </NavLink>
         </i>
       </nav>
     </div>
