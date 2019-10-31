6384ac401426369201f45c37708aed79e06e6419
alaa-yasin <alaayasin.ps@gmail.com>
2019-10-31 13:44:20 +0200

    fix the code Relates #57

--- src/components/navigationBar/index.jsx
+++ src/components/navigationBar/index.jsx
@@ -10,35 +10,57 @@ import './navigation.css';
 
 const NavBar = () => {
   return (
-    <div className="navbar__container">
-      <nav className="navbar">
-        <i className="navbar__icon">
-          <NavLink to={ROUTES.HOME} activeClassName="clicked">
-            <HomeIcon className="icons navbar__home-icon" />
-          </NavLink>
-        </i>
-        <i className="navbar__icon">
-          <NavLink to={ROUTES.HEAT_MAP} activeClassName="clicked">
-            <Calendar className="icons navbar__calendar" />
-          </NavLink>
-        </i>
-        <i className="navbar__icon">
-          <NavLink to={ROUTES.QUESTION} activeClassName="clicked">
-            <AddNew className="icons navbar__add-icon" />
-          </NavLink>
-        </i>
-        <i className="navbar__icon">
-          <NavLink to={ROUTES.FEED} activeClassName="clicked">
-            <FeedsFilter className="icons navbar__feeds-filter" />
-          </NavLink>
-        </i>
-        <i className="navbar__icon">
-          <NavLink to={ROUTES.ACCOUNT_SETTINGS} activeClassName="clicked">
-            <Settings className="icons navbar__settings" />
-          </NavLink>
-        </i>
-      </nav>
-    </div>
+    <footer aria-label="page footer">
+      <div className="navbar__container">
+        <nav className="navbar">
+          <i className="navbar__icon">
+            <NavLink
+              to={ROUTES.HOME}
+              activeClassName="clicked"
+              aria-label="go to home page"
+            >
+              <HomeIcon className="icons navbar__home-icon" />
+            </NavLink>
+          </i>
+          <i className="navbar__icon">
+            <NavLink
+              to={ROUTES.HEAT_MAP}
+              activeClassName="clicked"
+              aria-label="go to heat-map page"
+            >
+              <Calendar className="icons navbar__calendar" />
+            </NavLink>
+          </i>
+          <i className="navbar__icon">
+            <NavLink
+              to={ROUTES.QUESTION}
+              activeClassName="clicked"
+              aria-label="go to questions page"
+            >
+              <AddNew className="icons navbar__add-icon" />
+            </NavLink>
+          </i>
+          <i className="navbar__icon">
+            <NavLink
+              to={ROUTES.FEED}
+              activeClassName="clicked"
+              aria-label="go to feed page"
+            >
+              <FeedsFilter className="icons navbar__feeds-filter" />
+            </NavLink>
+          </i>
+          <i className="navbar__icon">
+            <NavLink
+              to={ROUTES.ACCOUNT_SETTINGS}
+              activeClassName="clicked"
+              aria-label="go to your account settings page"
+            >
+              <Settings className="icons navbar__settings" />
+            </NavLink>
+          </i>
+        </nav>
+      </div>
+    </footer>
   );
 };
 
