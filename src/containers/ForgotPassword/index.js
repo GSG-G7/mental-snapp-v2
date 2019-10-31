44e63ab58a99522598f69ff2aa2f74c47e283de7
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-10-31 15:53:12 +0200

    handled the change of the input

--- src/containers/ForgotPassword/index.js
+++ src/containers/ForgotPassword/index.js
@@ -1,6 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ForgotPassword from './forgotPass';
+import schema from './utils/schema';
 
-const index = props => <ForgotPassword {...props} />;
+export default class index extends Component {
+  // eslint-disable-next-line react/state-in-constructor
+  state = {
+    email: '',
+    error: '',
+  };
 
-export default index;
+  handleChange = ({ target: { value } }) => {
+    const { email } = this.state;
+    this.setState({ email: value }, async () => {
+      const valid = await schema.isValid({ email: email.trim() });
+      if (!valid) {
+        this.setState({ error: 'the input is not a valid email' });
+      } else {
+        this.setState({ error: '' });
+      }
+    });
+  };
+
+  handleClick = () => {
+    // Logic will be added hereW
+  };
+
+  render() {
+    const { email, error } = this.state;
+    const { ...props } = this.props;
+    return (
+      <ForgotPassword
+        email={email}
+        error={error}
+        handleChange={this.handleChange}
+        handleClick={this.handleClick}
+        {...props}
+      />
+    );
+  }
+}
