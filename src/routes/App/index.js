b6430f5c426df6e028e0b8ba952eea6ab1196889
AlaaTaima <alaa.ktaima@gmail.com>
2019-10-31 09:35:40 +0200

    edit style Relates #15

--- src/routes/App/index.js
+++ src/routes/App/index.js
@@ -1,63 +1,48 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
 
 import * as Containers from '../../containers';
 import 'antd/dist/antd.css';
 import './style.css';
+import Questions from '../../containers/Questions/index';
 
 const App = () => {
   return (
     <div className="App">
+      <Questions />
       <Router>
-        <Switch>
-          <Route exact path={ROUTES.LANDING} component={Containers.Landing} />
-          <Route exact path={ROUTES.HOME} component={Containers.Home} />
-          <Route exact path={ROUTES.SIGN_IN} component={Containers.SignIn} />
-          <Route exact path={ROUTES.SIGN_UP} component={Containers.SignUp} />
-          <Route exact path={ROUTES.ABOUT} component={Containers.About} />
-          <Route
-            exact
-            path={ROUTES.FORGOT_PASSWORD}
-            component={Containers.ForgotPassword}
-          />
-          <Route exact path={ROUTES.FEED} component={Containers.Feed} />
-          <Route exact path={ROUTES.HEAT_MAP} component={Containers.HeatMap} />
-          <Route
-            exact
-            path={ROUTES.ACCOUNT_SETTINGS}
-            component={Containers.AccountSettings}
-          />
-          <Route
-            exact
-            path={ROUTES.EDIT_ACCOUNT}
-            component={Containers.EditAccount}
-          />
-          <Route
-            exact
-            path={ROUTES.QUESTION}
-            component={Containers.Questions}
-          />
-          <Route
-            exact
-            path={ROUTES.CONFIRM_PASSWORD}
-            component={Containers.ConfirmPassword}
-          />
-          <Route exact path={ROUTES.JOURNAL} component={Containers.Journal} />
-          <Route
-            exact
-            path={ROUTES.UNAUTHENTICATED}
-            render={props => (
-              <Containers.Errors.AuthenticationError {...props} />
-            )}
-          />
-          <Route
-            exact
-            path={ROUTES.SERVER_ERROR}
-            component={Containers.Errors.ServerError}
-          />
-          <Route component={Containers.Errors.ClientError} />
-        </Switch>
+        <Route path={ROUTES.LANDING} component={Containers.Landing} />
+        <Route path={ROUTES.HOME} component={Containers.Home} />
+        <Route path={ROUTES.SIGN_IN} component={Containers.SignIn} />
+        <Route path={ROUTES.SIGN_UP} component={Containers.SignUp} />
+        <Route path={ROUTES.ABOUT} component={Containers.About} />
+        <Route
+          path={ROUTES.FORGOT_PASSWORD}
+          component={Containers.ForgotPassword}
+        />
+        <Route path={ROUTES.FEED} component={Containers.Feed} />
+        <Route path={ROUTES.HEAT_MAP} component={Containers.HeatMap} />
+        <Route
+          path={ROUTES.ACCOUNT_SETTINGS}
+          component={Containers.AccountSettings}
+        />
+        <Route path={ROUTES.EDIT_ACCOUNT} component={Containers.EditAccount} />
+        <Route path={ROUTES.QUESTION} component={Containers.Questions} />
+        <Route
+          path={ROUTES.CONFIRM_PASSWORD}
+          component={Containers.ConfirmPassword}
+        />
+        <Route path={ROUTES.JOURNAL} component={Containers.Journal} />
+        <Route
+          path={ROUTES.UNAUTHENTICATED}
+          component={Containers.Errors.AuthenticationError}
+        />
+        <Route
+          path={ROUTES.SERVER_ERROR}
+          component={Containers.Errors.ServerError}
+        />
+        <Route component={Containers.Errors.ClientError} />
       </Router>
     </div>
   );
