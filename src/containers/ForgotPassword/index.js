daee5bc26466425c2c121a2abac05613ffa77b14
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-10-31 11:35:51 +0200

    removed eslint comment

--- src/containers/ForgotPassword/index.js
+++ src/containers/ForgotPassword/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ForgotPassword from './forgotPass';
 
-// eslint-disable-next-line react/jsx-props-no-spreading
 const index = props => <ForgotPassword {...props} />;
 
 export default index;
