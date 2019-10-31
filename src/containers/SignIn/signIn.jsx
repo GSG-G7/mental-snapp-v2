7b1f71a24e8702fcfa305dc5ba042642d5f061f6
alaa-yasin <alaayasin.ps@gmail.com>
2019-10-31 11:16:11 +0200

    save files

--- src/containers/SignIn/signIn.jsx
+++ src/containers/SignIn/signIn.jsx
@@ -5,18 +5,25 @@ import './signIn.css';
 
 const SignIn = () => {
   return (
-    <div>
+    <div className="sign-in">
       <MainHeading text="Sign In" />
       <form>
         <Input
           type="email"
           className="sign-in__email-input"
-          placeholder="example@gmail.com"
+          placeholder="Email"
           prefix={
             <Icon type="mail" style={{ color: 'rgba(36, 36, 36, 0.4)' }} />
           }
         />
-        <Input type="password" className="sign-in__password-input" />
+        <Input
+          type="password"
+          className="sign-in__password-input"
+          placeholder="Password"
+          prefix={
+            <Icon type="lock" style={{ color: 'rgba(36, 36, 36, 0.4)' }} />
+          }
+        />
         <Button type="primary" className="signIn__btn">
           Sign In
         </Button>
