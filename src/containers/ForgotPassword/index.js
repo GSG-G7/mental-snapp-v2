a615a6c256d4b01215fbc2e69e4ce8e5b1b466fe
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-10-31 10:18:04 +0200

    changed the func from es5 to es6

--- src/containers/ForgotPassword/index.js
+++ src/containers/ForgotPassword/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ForgotPassword from './forgotPass';
 
-function index() {
-  return <ForgotPassword />;
-}
+const index = () => <ForgotPassword />;
 
 export default index;
