ad087a865b5ebd12075c181353a0c815369bfe21
AlaaTaima <alaa.ktaima@gmail.com>
2019-10-31 11:19:41 +0200

    pull master and fix router Relates #28

--- src/containers/Landing/index.js
+++ src/containers/Landing/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Button } from 'antd';
 import { ReactComponent as Illustration } from '../assets/images/landing.svg';
 import MainHeading from '../../components/MainHeading/index';
 import SubHeading from '../../components/subHeading/index';
+import { SIGN_UP, ABOUT, SIGN_IN } from '../../constants/routes';
 import './landing.css';
 
 const LandingPage = () => {
@@ -16,13 +18,19 @@ const LandingPage = () => {
         <div className="landing__content">
           <SubHeading text="Tell your story and positively guide your thoughts" />
         </div>
-        <Button type="primary" className="landing__button">
-          Sign In
-        </Button>
-        <p className="landing__link">Create a new account</p>
+        <Link to={SIGN_IN}>
+          <Button type="primary" className="landing__button">
+            Sign In
+          </Button>
+        </Link>
+        <Link to={SIGN_UP}>
+          <p className="landing__link">Create a new account</p>
+        </Link>
         <p className="landing__aboutLink">
           Read more about
-          <span className="landing__logo">Mental Snapp</span>
+          <Link to={ABOUT}>
+            <span className="landing__logo">Mental Snapp</span>
+          </Link>
         </p>
       </div>
     </div>
