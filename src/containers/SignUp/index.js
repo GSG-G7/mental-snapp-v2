0e06a004103750ba11cf85cac072db7ae70f0af6
fares98 <faresalhello21@gmail.com>
2019-11-03 10:10:12 +0200

    create signup page relates #9

--- src/containers/SignUp/index.js
+++ src/containers/SignUp/index.js
@@ -1,29 +1,3 @@
-import React, { Component } from 'react';
 import SignUpForm from './signUp';
 
-class SingUp extends Component {
-  state = {
-    error: '',
-  };
-
-  handleSubmit = newUser => {
-    // eslint-disable-next-line no-console
-    console.log(newUser);
-
-    // eslint-disable-next-line no-console
-    console.log('make a request');
-  };
-
-  render() {
-    const { error } = this.state;
-    return (
-      <SignUpForm
-        error={error}
-        handleSubmit={this.handleSubmit}
-        {...this.props}
-      />
-    );
-  }
-}
-
-export default SingUp;
+export default SignUpForm;
--- src/containers/SignUp/signUp.jsx
+++ src/containers/SignUp/signUp.jsx
@@ -1,101 +1,165 @@
-import React, { Component } from 'react';
-
-import { Input, Icon, Button } from 'antd';
+import React from 'react';
+import 'antd/dist/antd.css';
+import { Form, Input, Icon, Button } from 'antd';
 import PropTypes from 'prop-types';
-import Header from '../../components/Header';
-import GoogleButton from '../../components/GoogleButton';
-import FacebookButton from '../../components/FacebookButton';
-import schema from '../../utils/signup-validation';
 
+import Header from '../../components/Header';
+import FacebookBottun from '../../components/FacebookButton';
+import GoogleButton from '../../components/GoogleButton';
 import './signUp.css';
 
-class SignUp extends Component {
+const FormItem = Form.Item;
+
+class SignUpForm extends React.Component {
   state = {
-    name: '',
-    email: '',
-    password: '',
-    passwordConfirm: '',
+    confirmDirty: false,
   };
 
-  handleSubmit = async () => {
-    await schema.validate(this.state, { abortEarly: false });
+  handleSubmit = e => {
+    e.preventDefault();
+    const {
+      form: { validateFieldsAndScroll },
+    } = this.props;
+    validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
   };
 
-  handleChange = ({ target: { name, value } }) =>
-    this.setState({ [name]: value });
+  handleConfirmBlur = e => {
+    const { value } = e.target;
+    const { confirmDirty } = this.state;
+    this.setState({ confirmDirty: confirmDirty || !!value });
+  };
+
+  compareToFirstPassword = (rule, value, callback) => {
+    const { form } = this.props;
+    if (value && value !== form.getFieldValue('password')) {
+      callback('Two passwords that you enter is inconsistent!');
+    } else {
+      callback();
+    }
+  };
 
   render() {
     const {
+      form: { getFieldDecorator },
+    } = this.props;
+    const {
       history: { goBack },
     } = this.props;
-    const { name, password, passwordConfirm, email } = this.state;
     return (
       <div className="signup">
         <Header text="Sign Up" handleBack={goBack} />
 
-        <form className="signup__form" onSubmit={this.handleSubmit}>
-          <Input
-            prefix={<Icon type="user" className="signup__icon" />}
-            className="signup__input"
-            placeholder="Name"
-            name="name"
-            value={name}
-            onChange={this.handleChange}
-          />
+        <section className="signup__form">
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Item hasFeedback>
+              {getFieldDecorator('username', {
+                rules: [
+                  {
+                    required: true,
+                    message: 'Please input your Name!',
+                  },
+                ],
+              })(
+                <Input
+                  prefix={<Icon type="user" className="signup__icon" />}
+                  placeholder="Name"
+                />
+              )}
+            </Form.Item>
 
-          <Input
-            prefix={<Icon type="mail" className="signup__icon" />}
-            className="signup__input"
-            placeholder="Email"
-            name="email"
-            value={email}
-            onChange={this.handleChange}
-          />
+            <Form.Item hasFeedback>
+              {getFieldDecorator('email', {
+                rules: [
+                  {
+                    type: 'email',
+                    message: 'The input is not valid E-mail!',
+                  },
+                  {
+                    required: true,
+                    message: 'Please input your E-mail!',
+                  },
+                ],
+              })(
+                <Input
+                  prefix={<Icon type="mail" className="signup__icon" />}
+                  placeholder="Email"
+                />
+              )}
+            </Form.Item>
 
-          <Input
-            prefix={<Icon type="lock" className="signup__icon" />}
-            className="signup__input"
-            placeholder="Password"
-            name="password"
-            value={password}
-            onChange={this.handleChange}
-            type="password"
-          />
+            <Form.Item hasFeedback>
+              {getFieldDecorator('password', {
+                rules: [
+                  {
+                    required: true,
+                    message: 'Please input your password!',
+                  },
+                  {
+                    validator: this.validateToNextPassword,
+                  },
+                ],
+              })(
+                <Input.Password
+                  prefix={<Icon type="lock" className="signup__icon" />}
+                  placeholder="Password"
+                />
+              )}
+            </Form.Item>
 
-          <Input
-            prefix={<Icon type="check-circle" className="signup__icon" />}
-            className="signup__input"
-            placeholder="Confirm Password"
-            name="passwordConfirm"
-            value={passwordConfirm}
-            onChange={this.handleChange}
-            type="password"
-          />
+            <Form.Item hasFeedback>
+              {getFieldDecorator('confirm', {
+                rules: [
+                  {
+                    required: true,
+                    message: 'Please confirm your password!',
+                  },
+                  {
+                    validator: this.compareToFirstPassword,
+                  },
+                ],
+              })(
+                <Input.Password
+                  onBlur={this.handleConfirmBlur}
+                  prefix={<Icon type="check-circle" className="signup__icon" />}
+                  placeholder="Password"
+                />
+              )}
+            </Form.Item>
 
-          <Button
-            type="primary"
-            onClick={this.handleSubmit}
-            size="large"
-            className="signup__submit-btn"
-          >
-            Create Account
-          </Button>
+            <FormItem>
+              <Button type="primary" htmlType="submit">
+                Sign Up
+              </Button>
+            </FormItem>
+          </Form>
+        </section>
 
-          <p className="signup__or">OR</p>
+        <section className="signup__or">OR</section>
 
-          <div className="signup__buttons">
-            <GoogleButton />
-            <FacebookButton />
-          </div>
-        </form>
+        <section className="signup__buttons">
+          <FacebookBottun />
+          <GoogleButton />
+        </section>
       </div>
     );
   }
 }
 
-SignUp.propTypes = {
+const HorizontalSignUp = Form.create({ name: 'sign up' })(SignUpForm);
+
+SignUpForm.propTypes = {
+  form: PropTypes.shape({
+    validateFieldsAndScroll: PropTypes.func.isRequired,
+    getFieldValue: PropTypes.func.isRequired,
+    getFieldDecorator: PropTypes.func.isRequired,
+  }).isRequired,
   history: PropTypes.shape({
     goBack: PropTypes.func.isRequired,
   }).isRequired,
 };
-export default SignUp;
+
+export default HorizontalSignUp;
