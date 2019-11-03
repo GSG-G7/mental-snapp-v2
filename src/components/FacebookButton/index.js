53a0bd55d8861d1525a86d7e6e1e2833182df1ec
fares98 <faresalhello21@gmail.com>
2019-11-03 13:23:02 +0200

    add a function relates #9

--- src/components/FacebookButton/index.js
+++ src/components/FacebookButton/index.js
@@ -3,9 +3,13 @@ import React from 'react';
 import { ReactComponent as FacebookImg } from '../../containers/assets/images/facebook.svg';
 import './style.css';
 
+const handleClick = () => {
+  // handle login or sign up with facebook
+};
+
 const Facebook = () => {
   return (
-    <button type="submit" className="facebook-btn">
+    <button type="submit" className="facebook-btn" onClick={handleClick}>
       <FacebookImg className="facebook-btn__img" />
       <span className="facebook-btn__text">Facebook</span>
     </button>
