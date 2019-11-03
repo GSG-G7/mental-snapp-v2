b4a49b1916a7c4feb35ba5553670e439ee6e2b5a
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-11-03 14:10:39 +0200

    exported the component and removed the state

--- src/containers/ForgotPassword/index.js
+++ src/containers/ForgotPassword/index.js
@@ -1,40 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import ForgotPassword from './forgotPass';
-import schema from '../../utils/schema';
 
-export default class index extends Component {
-  state = {
-    email: '',
-    error: '',
-  };
-
-  handleChange = ({ target: { value } }) => {
-    const { email } = this.state;
-    this.setState({ email: value }, async () => {
-      const valid = await schema.isValid({ email: email.trim() });
-      if (!valid) {
-        this.setState({ error: 'Please, enter a valid email' });
-      } else {
-        this.setState({ error: '' });
-      }
-    });
-  };
-
-  handleClick = () => {
-    // Here we need to use the firebase forget password function
-  };
-
-  render() {
-    const { email, error } = this.state;
-    return (
-      <ForgotPassword
-        email={email}
-        error={error}
-        handleChange={this.handleChange}
-        handleClick={this.handleClick}
-        {...this.props}
-      />
-    );
-  }
-}
+export default props => <ForgotPassword {...props} />;
