5be89e9bff4ae892bf042fc8d047b90d25f369bd
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-11-04 14:50:38 +0200

    added a state for keeping the data and journals

--- src/containers/HeatMap/index.js
+++ src/containers/HeatMap/index.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 
 import HeatMap from './heatMap';
-import data from './data';
+import fakeData from './data';
 
 class index extends Component {
-  state = {};
+  state = {
+    data: [...fakeData],
+  };
+
+  // This function should show data based on day
+  handleClick = value => {
+    // the date of the clicked day in the same way it's stored in our DB
+    if (value) {
+      const currentDay = new Date(value.date).toISOString();
+    }
+  };
 
   render() {
-    return <HeatMap />;
+    const { data } = this.state;
+    return <HeatMap data={data} handleClick={this.handleClick} />;
   }
 }
 
