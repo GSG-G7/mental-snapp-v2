730c29d8f2775f8f2ecd9377266e5bbc5f994a31
fares98 <faresalhello21@gmail.com>
2019-11-04 10:41:40 +0200

    general edites relates #9

--- src/components/MainHeading/index.jsx
+++ src/components/MainHeading/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const MainHeading = ({ text, className }) => (
-  <h3 className={className}>{text}</h3>
+  <h3 className={`main-head ${className}`}>{text}</h3>
 );
 
 MainHeading.propTypes = {
