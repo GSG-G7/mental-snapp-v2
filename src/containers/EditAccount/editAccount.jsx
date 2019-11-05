c0dc2879c7bf7aa9d05444cd6aefd8d782ab73f5
fares98 <faresalhello21@gmail.com>
2019-11-05 10:49:19 +0200

    convert the class to function :( relates #23

--- src/containers/EditAccount/editAccount.jsx
+++ src/containers/EditAccount/editAccount.jsx
@@ -1,4 +1,142 @@
-import React, { Component } from 'react';
+/* eslint-disable no-undef */
+// import React, { Component } from 'react';
+
+// import { Form, Input, Icon, Button, Checkbox } from 'antd';
+// import PropTypes from 'prop-types';
+
+// import Header from '../../components/Header';
+// import './editAccount.css';
+
+// class EditAccount extends Component {
+//   state = {
+//     checked: false,
+//     info: { name: 'Fares', email: 'fares@gmail.com' },
+//   };
+
+//   onChange = e => {
+//     this.setState({
+//       checked: e.target.checked,
+//     });
+//   };
+
+//   render() {
+//     const {
+//       checked,
+//       info: { email, name },
+//     } = this.state;
+
+//     const {
+//       form: { getFieldDecorator, validateFieldsAndScroll },
+//       history: { goBack },
+//     } = this.props;
+
+//     const handleSubmit = e => {
+//       e.preventDefault();
+//       validateFieldsAndScroll((err, values) => {
+//         if (!err) {
+//           // eslint-disable-next-line no-console
+//           console.log('Received values of form: ', values);
+//         }
+//       });
+//     };
+//     return (
+//       <div className="edit-account">
+//         <Header text="Edit Account" handleBack={goBack} />
+
+//         <section className="edit-account__form">
+//           <Form onSubmit={handleSubmit}>
+//             <Form.Item hasFeedback>
+//               {getFieldDecorator('name', {
+//                 initialValue: `${name}`,
+//                 rules: [
+//                   {
+//                     required: true,
+//                     message: 'Please input your Name!',
+//                   },
+//                 ],
+//               })(
+//                 <Input
+//                   prefix={<Icon type="user" className="edit-account__icon" />}
+//                   placeholder="Name"
+//                 />
+//               )}
+//             </Form.Item>
+
+//             <Form.Item hasFeedback>
+//               {getFieldDecorator('email', {
+//                 initialValue: `${email}`,
+
+//                 rules: [
+//                   {
+//                     type: 'email',
+//                     message: 'The input is not valid E-mail!',
+//                   },
+//                   {
+//                     required: true,
+//                     message: 'Please input your E-mail!',
+//                   },
+//                 ],
+//               })(
+//                 <Input
+//                   prefix={<Icon type="mail" className="edit-account__icon" />}
+//                   placeholder="Email"
+//                 />
+//               )}
+//             </Form.Item>
+
+//             <section className="edit-account__checkbox">
+//               <Checkbox checked={checked} onChange={this.onChange}>
+//                 Change Password
+//               </Checkbox>
+//             </section>
+
+//             <Form.Item
+//               hasFeedback
+//               className={checked ? 'transition' : 'transition hidden'}
+//             >
+//               {getFieldDecorator('password', {
+//                 rules: [
+//                   {
+//                     required: checked,
+//                     message: 'Enter your password',
+//                   },
+//                 ],
+//               })(
+//                 <Input.Password
+//                   prefix={<Icon type="lock" className="edit-account__icon" />}
+//                   placeholder="Password"
+//                 />
+//               )}
+//             </Form.Item>
+
+//             <Form.Item>
+//               <Button type="primary" htmlType="submit">
+//                 Save Changes
+//               </Button>
+//             </Form.Item>
+//           </Form>
+//         </section>
+//       </div>
+//     );
+//   }
+// }
+
+// const editAccount = Form.create({ name: 'Edit Account' })(EditAccount);
+
+// EditAccount.propTypes = {
+//   form: PropTypes.shape({
+//     validateFieldsAndScroll: PropTypes.func.isRequired,
+//     getFieldValue: PropTypes.func.isRequired,
+//     getFieldDecorator: PropTypes.func.isRequired,
+//   }).isRequired,
+//   history: PropTypes.shape({
+//     goBack: PropTypes.func.isRequired,
+//   }).isRequired,
+// };
+
+// export default editAccount;
+
+import React from 'react';
 
 import { Form, Input, Icon, Button, Checkbox } from 'antd';
 import PropTypes from 'prop-types';
@@ -6,110 +144,107 @@ import PropTypes from 'prop-types';
 import Header from '../../components/Header';
 import './editAccount.css';
 
-class EditAccount extends Component {
-  state = {
-    checked: false,
-  };
+const EditAccount = props => {
+  const {
+    info: { name },
+  } = props;
+  const email = 'Fares@gmail.com';
+  const checked = false;
 
-  toggleChecked = () => {
-    const { checked } = this.state;
-    this.setState({ checked: !checked });
-  };
+  const {
+    form: { getFieldDecorator, validateFieldsAndScroll },
+    history: { goBack },
+  } = props;
 
-  onChange = e => {
-    this.setState({
-      checked: e.target.checked,
+  const handleSubmit = e => {
+    e.preventDefault();
+    validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        // eslint-disable-next-line no-console
+        console.log('Received values of form: ', values);
+      }
     });
   };
+  return (
+    <div className="edit-account">
+      <Header text="Edit Account" handleBack={goBack} />
 
-  render() {
-    const { checked } = this.state;
-    const handleSubmit = e => {
-      e.preventDefault();
-      // submit the data
-    };
-    const {
-      form: { getFieldDecorator },
-      history: { goBack },
-    } = this.props;
-    return (
-      <div className="edit-account">
-        <Header text="Edit Account" handleBack={goBack} />
+      <section className="edit-account__form">
+        <Form onSubmit={handleSubmit}>
+          <Form.Item hasFeedback>
+            {getFieldDecorator('name', {
+              initialValue: `${name}`,
+              rules: [
+                {
+                  required: true,
+                  message: 'Please input your Name!',
+                },
+              ],
+            })(
+              <Input
+                prefix={<Icon type="user" className="edit-account__icon" />}
+                placeholder="Name"
+              />
+            )}
+          </Form.Item>
 
-        <section className="edit-account__form">
-          <Form onSubmit={handleSubmit}>
-            <Form.Item hasFeedback>
-              {getFieldDecorator('name', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Please input your Name!',
-                  },
-                ],
-              })(
-                <Input
-                  prefix={<Icon type="user" className="edit-account__icon" />}
-                  placeholder="Name"
-                />
-              )}
-            </Form.Item>
+          <Form.Item hasFeedback>
+            {getFieldDecorator('email', {
+              initialValue: `${email}`,
 
-            <Form.Item hasFeedback>
-              {getFieldDecorator('email', {
-                rules: [
-                  {
-                    type: 'email',
-                    message: 'The input is not valid E-mail!',
-                  },
-                  {
-                    required: true,
-                    message: 'Please input your E-mail!',
-                  },
-                ],
-              })(
-                <Input
-                  prefix={<Icon type="mail" className="edit-account__icon" />}
-                  placeholder="Email"
-                />
-              )}
-            </Form.Item>
+              rules: [
+                {
+                  type: 'email',
+                  message: 'The input is not valid E-mail!',
+                },
+                {
+                  required: true,
+                  message: 'Please input your E-mail!',
+                },
+              ],
+            })(
+              <Input
+                prefix={<Icon type="mail" className="edit-account__icon" />}
+                placeholder="Email"
+              />
+            )}
+          </Form.Item>
 
-            <section className="edit-account__checkbox">
-              <Checkbox checked={checked} onChange={this.onChange}>
-                Change Password
-              </Checkbox>
-            </section>
+          <section className="edit-account__checkbox">
+            <Checkbox checked={checked} onChange={onChange}>
+              Change Password
+            </Checkbox>
+          </section>
 
-            <Form.Item
-              hasFeedback
-              className={!checked ? 'transition hidden' : 'transition'}
-            >
-              {getFieldDecorator('password', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Enter your password',
-                  },
-                ],
-              })(
-                <Input.Password
-                  prefix={<Icon type="lock" className="edit-account__icon" />}
-                  placeholder="Password"
-                />
-              )}
-            </Form.Item>
+          <Form.Item
+            hasFeedback
+            className={checked ? 'transition' : 'transition hidden'}
+          >
+            {getFieldDecorator('password', {
+              rules: [
+                {
+                  required: checked,
+                  message: 'Enter your password',
+                },
+              ],
+            })(
+              <Input.Password
+                prefix={<Icon type="lock" className="edit-account__icon" />}
+                placeholder="Password"
+              />
+            )}
+          </Form.Item>
 
-            <Form.Item>
-              <Button type="primary" htmlType="submit">
-                Save Changes
-              </Button>
-            </Form.Item>
-          </Form>
-        </section>
-      </div>
-    );
-  }
-}
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Save Changes
+            </Button>
+          </Form.Item>
+        </Form>
+      </section>
+    </div>
+  );
+};
 
 const editAccount = Form.create({ name: 'Edit Account' })(EditAccount);
 
@@ -121,6 +256,9 @@ EditAccount.propTypes = {
   }).isRequired,
   history: PropTypes.shape({
     goBack: PropTypes.func.isRequired,
+  }).isRequired,
+  info: PropTypes.shape({
+    name: PropTypes.string.isRequired,
   }).isRequired,
 };
 
--- src/containers/EditAccount/index.js
+++ src/containers/EditAccount/index.js
@@ -1,3 +1,23 @@
+import React, { Component } from 'react';
 import EditAccount from './editAccount';
 
-export default EditAccount;
+class Edit extends Component {
+  state = {
+    // eslint-disable-next-line react/no-unused-state
+    info: {
+      name: 'Fares',
+      email: 'fares@gmail.com',
+    },
+    // eslint-disable-next-line react/no-unused-state
+    checked: false,
+  };
+
+  // eslint-disable-next-line react/no-unused-state
+  onChange = ({ target: { checked } }) => this.setState({ checked });
+
+  render() {
+    return <EditAccount state={this.state} handleChange={this.onChange} />;
+  }
+}
+
+export default Edit;
