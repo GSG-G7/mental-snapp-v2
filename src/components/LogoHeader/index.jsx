78aee105b093a876d7de416254bff2e628d27cb2
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-11-05 10:40:38 +0200

    added the class to the link not the child

--- src/components/LogoHeader/index.jsx
+++ src/components/LogoHeader/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import logo from '../assets/images/logo.png';
+import logo from '../assets/images/logo1.png';
 import './style.css';
 
 const index = () => {
   return (
     <div className="logo-header">
       <img src={logo} alt="mental snapp logo" className="logo-header__image" />
-      <Link to="/about">
-        <span className="logo-header__link">about</span>
+      <Link className="logo-header__link" to="/about">
+        <span>about</span>
       </Link>
     </div>
   );
