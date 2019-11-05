7dc8f1987557cf3e0e5ba36cf9fc62d2103290d9
AlaaTaima <alaa.ktaima@gmail.com>
2019-11-05 13:10:53 +0200

    refactor code

--- src/containers/Questions/index.js
+++ src/containers/Questions/index.js
@@ -1,3 +1,139 @@
-import Questions from './questions';
+import React from 'react';
+import propTypes from 'prop-types';
+import { message } from 'antd';
+import Question from './questions';
+
+import schema from './questionValidation';
+
+const entryData = [
+  {
+    id: 1,
+    heading: 'Today, Iam grateful for',
+    percent: 34,
+  },
+  {
+    id: 2,
+    heading: 'Challenge I face',
+    percent: 68,
+  },
+  {
+    id: 3,
+    heading: 'Iam looking to develop',
+    percent: 100,
+  },
+];
+
+class Questions extends React.Component {
+  state = {
+    current: 0,
+    title: '',
+    content: '',
+    errors: {},
+    journals: [],
+  };
+
+  handleConfirm = e => {
+    message.warning("You didn't make an entry today");
+    const { history } = this.props;
+    history.push('/home');
+  };
+
+  handleChange = ({ target: { value, name } }) => {
+    this.setState({ [name]: value, errors: {} });
+  };
+
+  handleNext = async () => {
+    const { current, title, content, journals } = this.state;
+    try {
+      await schema.validate({ title, content }, { abortEarly: false });
+      journals.push({ title, content });
+      return this.setState({
+        current: current + 1,
+        content: '',
+        title: '',
+        errors: {},
+      });
+    } catch (error) {
+      const objError = {};
+      error.inner.forEach(fielderror => {
+        objError[fielderror.path] = fielderror.message;
+      });
+      return this.setState({ errors: objError });
+    }
+  };
+
+  handleSubmit = async () => {
+    const { title, content, journals } = this.state;
+    const { history } = this.props;
+    try {
+      await schema.validate({ title, content }, { abortEarly: false });
+      journals.push({ title, content });
+      message.success('Yes, you have added a journal');
+      history.push('/home');
+      // here, a request will be post to firebase to save data
+      // that will be as follows : [{title:'', content:'', time:'', date:'',month:''}]
+      return this.setState({ journals: [] });
+    } catch (error) {
+      const objError = {};
+      error.inner.forEach(fielderror => {
+        objError[fielderror.path] = fielderror.message;
+      });
+      return this.setState({ errors: objError });
+    }
+  };
+
+  handlePrev = () => {
+    const { current } = this.state;
+    const curr = current - 1;
+    this.setState({ current: curr, errors: {} });
+  };
+
+  handleSkip = () => {
+    const { current, journals } = this.state;
+    if (current < entryData.length - 1) {
+      const curr = current + 1;
+      this.setState({ current: curr, errors: {} });
+    } else {
+      const { history } = this.props;
+      if (journals.length !== 0) {
+        message.success('Yes, you have added a journal');
+        history.push('/home');
+        // here, a request will be post to firebase to save data
+        // that will be as follows : [{title:'', content:'', time:'', date:'',month:''}]
+        this.setState({ journals: [] });
+      } else {
+        message.warning("You didn't make an entry today");
+        history.push('/home');
+      }
+    }
+  };
+
+  render() {
+    const {
+      history: { goBack },
+    } = this.props;
+    const { errors } = this.state;
+    return (
+      <Question
+        state={this.state}
+        handleNext={this.handleNext}
+        handlePrev={this.handlePrev}
+        handleSubmit={this.handleSubmit}
+        handleChange={this.handleChange}
+        handleSkip={this.handleSkip}
+        handleGoBack={goBack}
+        handleConfirm={this.handleConfirm}
+        errors={errors}
+      />
+    );
+  }
+}
+
+Questions.propTypes = {
+  history: propTypes.shape({
+    push: propTypes.func.isRequired,
+    goBack: propTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Questions;
--- src/containers/Questions/questions.jsx
+++ src/containers/Questions/questions.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 
 import propTypes from 'prop-types';
 
-import { Button, message, Input, Progress, Icon, Popconfirm } from 'antd';
+import { Button, Input, Progress, Icon, Popconfirm } from 'antd';
 
 import BackButton from '../../components/BackButton';
-import schema from './questionValidation';
 import './questions.css';
 
 const { TextArea } = Input;
@@ -28,179 +27,113 @@ const entryData = [
   },
 ];
 
-class Questions extends React.Component {
-  state = {
-    current: 0,
-    title: '',
-    content: '',
-    errors: {},
-    journals: [],
-  };
+const Question = props => {
+  const {
+    handleNext,
+    handlePrev,
+    handleSubmit,
+    handleChange,
+    handleSkip,
+    handleGoBack,
+    handleConfirm,
+    state: { current, title, content, errors },
+  } = props;
 
-  confirm = e => {
-    message.warning("You didn't make an entry today");
-    const { history } = this.props;
-    history.push('/home');
-  };
-
-  handleChange = ({ target: { value, name } }) => {
-    this.setState({ [name]: value, errors: {} });
-  };
-
-  next = async () => {
-    const { current, title, content, journals } = this.state;
-    try {
-      await schema.validate({ title, content }, { abortEarly: false });
-      journals.push({ title, content });
-      return this.setState({
-        current: current + 1,
-        content: '',
-        title: '',
-        errors: {},
-      });
-    } catch (error) {
-      const objError = {};
-      error.inner.forEach(fielderror => {
-        objError[fielderror.path] = fielderror.message;
-      });
-      return this.setState({ errors: objError });
-    }
-  };
-
-  finish = async () => {
-    const { title, content, journals } = this.state;
-    const { history } = this.props;
-    try {
-      await schema.validate({ title, content }, { abortEarly: false });
-      journals.push({ title, content });
-      message.success('Yes, you have added a journal');
-      history.push('/home');
-      // here, a request will be post to firebase to save data
-      // that will be as follows : [{title:'', content:'', time:'', date:'',month:''}]
-      return this.setState({ journals: [] });
-    } catch (error) {
-      const objError = {};
-      error.inner.forEach(fielderror => {
-        objError[fielderror.path] = fielderror.message;
-      });
-      return this.setState({ errors: objError });
-    }
-  };
-
-  prev = () => {
-    const { current } = this.state;
-    const curr = current - 1;
-    this.setState({ current: curr, errors: {} });
-  };
-
-  skip = () => {
-    const { current, journals } = this.state;
-    if (current < entryData.length - 1) {
-      const curr = current + 1;
-      this.setState({ current: curr, errors: {} });
-    } else {
-      const { history } = this.props;
-      if (journals.length !== 0) {
-        message.success('Yes, you have added a journal');
-        history.push('/home');
-        // here, a request will be post to firebase to save data
-        // that will be as follows : [{title:'', content:'', time:'', date:'',month:''}]
-        this.setState({ journals: [] });
-      } else {
-        message.warning("You didn't make an entry today");
-        history.push('/home');
-      }
-    }
-  };
-
-  render() {
-    const { errors, current, title, content } = this.state;
-    const {
-      history: { goBack },
-    } = this.props;
-    return (
-      <div>
-        <div className="question__navigation">
-          {current > 0 ? (
-            <Icon type="left" onClick={() => this.prev()} />
-          ) : (
-            <BackButton handleBack={goBack} />
-          )}
-          <div>
-            <Popconfirm
-              title="Do you really want to exit?"
-              onConfirm={this.confirm}
-              okText="Yes"
-              cancelText="cancel"
-            >
-              <Icon type="close" />
-            </Popconfirm>
-          </div>
-        </div>
-        <div className="question__type">
-          <p>
-            Question
-            <span className="question__count">
-              {current + 1}
-              /3
-            </span>
-          </p>
-          <Progress
-            percent={entryData[current].percent}
-            size="small"
-            showInfo={false}
-          />
-          <p>{entryData[current].heading}</p>
-          <div className="question__title">
-            <Input
-              placeholder="Title"
-              name="title"
-              value={title}
-              autoComplete="off"
-              onChange={this.handleChange}
-            />
-            {errors.title && (
-              <span className="question__error-field">{errors.title}</span>
-            )}
-          </div>
-          <div className="question__content">
-            <TextArea
-              rows={4}
-              placeholder="Write your words"
-              name="content"
-              value={content}
-              onChange={this.handleChange}
-            />
-            {errors.content && (
-              <span className="question__error-field">{errors.content}</span>
-            )}
-          </div>
-        </div>
-
-        <div className="question__steps-action">
-          {current < entryData.length - 1 ? (
-            <Button type="primary" onClick={() => this.next()}>
-              Next
-            </Button>
-          ) : (
-            <Button type="primary" onClick={() => this.finish()}>
-              Done
-            </Button>
-          )}
-
-          <Button style={{ marginLeft: 8 }} onClick={() => this.skip()}>
-            Skip
-          </Button>
+  return (
+    <div>
+      <div className="question__navigation">
+        {current > 0 ? (
+          <Icon type="left" onClick={handlePrev} />
+        ) : (
+          <BackButton handleBack={handleGoBack} />
+        )}
+        <div>
+          <Popconfirm
+            title="Do you really want to exit?"
+            onConfirm={handleConfirm}
+            okText="Yes"
+            cancelText="cancel"
+          >
+            <Icon type="close" />
+          </Popconfirm>
         </div>
       </div>
-    );
-  }
-}
+      <div className="question__type">
+        <p>
+          Question
+          <span className="question__count">
+            {current + 1}
+            /3
+          </span>
+        </p>
+        <Progress
+          percent={entryData[current].percent}
+          size="small"
+          showInfo={false}
+        />
+        <p>{entryData[current].heading}</p>
+        <div className="question__title">
+          <Input
+            placeholder="Title"
+            name="title"
+            value={title}
+            autoComplete="off"
+            onChange={handleChange}
+          />
+          {errors.title && (
+            <span className="question__error-field">{errors.title}</span>
+          )}
+        </div>
+        <div className="question__content">
+          <TextArea
+            rows={4}
+            placeholder="Write your words"
+            name="content"
+            value={content}
+            onChange={handleChange}
+          />
+          {errors.content && (
+            <span className="question__error-field">{errors.content}</span>
+          )}
+        </div>
+      </div>
 
-Questions.propTypes = {
+      <div className="question__steps-action">
+        {current < entryData.length - 1 ? (
+          <Button type="primary" onClick={handleNext}>
+            Next
+          </Button>
+        ) : (
+          <Button type="primary" onClick={handleSubmit}>
+            Done
+          </Button>
+        )}
+
+        <Button style={{ marginLeft: 8 }} onClick={handleSkip}>
+          Skip
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+Question.propTypes = {
   history: propTypes.shape({
     push: propTypes.func.isRequired,
     goBack: propTypes.func.isRequired,
   }).isRequired,
+  handleNext: propTypes.func.isRequired,
+  handlePrev: propTypes.func.isRequired,
+  handleSubmit: propTypes.func.isRequired,
+  handleChange: propTypes.func.isRequired,
+  handleSkip: propTypes.func.isRequired,
+  handleGoBack: propTypes.func.isRequired,
+  handleConfirm: propTypes.func.isRequired,
+  state: propTypes.shape({
+    current: propTypes.number.isRequired,
+    title: propTypes.string.isRequired,
+    content: propTypes.string.isRequired,
+    errors: propTypes.object.isRequired,
+  }).isRequired,
 };
-export default Questions;
+export default Question;
