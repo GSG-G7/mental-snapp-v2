a5069f9993f8fc6aab0bc87cf8f15a2e218fefb7
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-11-06 14:40:15 +0200

    modefied the fake data

--- src/containers/HeatMap/data.js
+++ src/containers/HeatMap/data.js
@@ -9,58 +9,6 @@ export const heatMapData = [
   },
   {
     date: '2019-11-03T22:00:00.000Z',
-    count: 6,
-  },
-  {
-    date: '2019-11-04T22:00:00.000Z',
-    count: 3,
-  },
-  {
-    date: '2019-11-05T22:00:00.000Z',
-    count: 2,
-  },
-  {
-    date: '2019-11-06T22:00:00.000Z',
-    count: 4,
-  },
-  {
-    date: '2019-11-07T22:00:00.000Z',
-    count: 1,
-  },
-  {
-    date: '2019-11-08T22:00:00.000Z',
-    count: 0,
-  },
-  {
-    date: '2019-11-09T22:00:00.000Z',
-    count: 2,
-  },
-  {
-    date: '2019-11-10T22:00:00.000Z',
-    count: 3,
-  },
-  {
-    date: '2019-11-11T22:00:00.000Z',
-    count: 7,
-  },
-  {
-    date: '2019-11-12T22:00:00.000Z',
-    count: 3,
-  },
-  {
-    date: '2019-11-13T22:00:00.000Z',
-    count: 1,
-  },
-  {
-    date: '2019-11-14T22:00:00.000Z',
-    count: 5,
-  },
-  {
-    date: '2019-11-15T22:00:00.000Z',
-    count: 1,
-  },
-  {
-    date: '2019-11-16T22:00:00.000Z',
     count: 4,
   },
 ];
@@ -80,7 +28,7 @@ export const journalsData = [
       title: 'reading more books',
       body: 'some dummy and very stupid data',
     },
-    timestamp: '2019-10-30T09:17:27.037Z', // It will be stored in DB as an ISO string
+    timestamp: '2019-11-01T22:00:00.000Z', // It will be stored in DB as an ISO string
   },
   {
     id: '2312ASADSD46556',
@@ -96,6 +44,86 @@ export const journalsData = [
       title: 'reading more books',
       body: 'some dummy and very stupid data',
     },
-    timestamp: '2019-10-30T09:17:27.037Z', // It will be stored in DB as an ISO string
+    timestamp: '2019-11-01T22:00:00.000Z', // It will be stored in DB as an ISO string
+  },
+  {
+    id: '2312ASADSD46556',
+    grateful: {
+      title: 'Family',
+      body: 'some dummy and very stupid data',
+    },
+    developing: {
+      title: 'not finding time',
+      body: 'some dummy and very stupid data',
+    },
+    challenge: {
+      title: 'reading more books',
+      body: 'some dummy and very stupid data',
+    },
+    timestamp: '2019-11-02T22:00:00.000Z', // It will be stored in DB as an ISO string
+  },
+  {
+    id: '2312ASAdSD46556',
+    grateful: {
+      title: 'Family',
+      body: 'some dummy and very stupid data',
+    },
+    developing: {
+      title: 'not finding time',
+      body: 'some dummy and very stupid data',
+    },
+    challenge: {
+      title: 'reading more books',
+      body: 'some dummy and very stupid data',
+    },
+    timestamp: '2019-11-03T22:00:00.000Z', // It will be stored in DB as an ISO string
+  },
+  {
+    id: '2312ASADSs46556',
+    grateful: {
+      title: 'Family',
+      body: 'some dummy and very stupid data',
+    },
+    developing: {
+      title: 'not finding time',
+      body: 'some dummy and very stupid data',
+    },
+    challenge: {
+      title: 'reading more books',
+      body: 'some dummy and very stupid data',
+    },
+    timestamp: '2019-11-03T22:00:00.000Z', // It will be stored in DB as an ISO string
+  },
+  {
+    id: '2312ASAD2D46556',
+    grateful: {
+      title: 'Family',
+      body: 'some dummy and very stupid data',
+    },
+    developing: {
+      title: 'not finding time',
+      body: 'some dummy and very stupid data',
+    },
+    challenge: {
+      title: 'reading more books',
+      body: 'some dummy and very stupid data',
+    },
+    timestamp: '2019-11-03T22:00:00.000Z', // It will be stored in DB as an ISO string
+  },
+  {
+    id: '2312ASADaD46556',
+    grateful: {
+      title: 'Family',
+      body: 'some dummy and very stupid data',
+    },
+    developing: {
+      title: 'not finding time',
+      body: 'some dummy and very stupid data',
+    },
+    challenge: {
+      title: 'reading more books',
+      body: 'some dummy and very stupid data',
+    },
+    timestamp: '2019-11-03T22:00:00.000Z', // It will be stored in DB as an ISO string
   },
 ];
