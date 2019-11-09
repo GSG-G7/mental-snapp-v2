801300b05e9d7bd2ff302833da56889213ddb10f
AlaaTaima <alaa.ktaima@gmail.com>
2019-11-10 01:47:26 +0200

    set authentication api and firestore Relates #93

--- src/containers/Firebase/firebase.js
+++ src/containers/Firebase/firebase.js
@@ -20,18 +20,6 @@ class Firebase {
     this.db = app.firestore();
   }
 
-  doCreateUserWithEmailAndPassword = (email, password) =>
-    this.auth.createUserWithEmailAndPassword(email, password);
-
-  doSignInWithEmailAndPassword = (email, password) =>
-    this.auth.signInWithEmailAndPassword(email, password);
-
-  doSignOut = () => this.auth.signOut();
-
-  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
-
-  doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
-
   user = uid => this.db.doc(`users/${uid}`);
 
   users = () => this.db.collection('users');
