bebe1accdd23141e7000832247f46c258924866f
fares98 <faresalhello21@gmail.com>
2019-11-11 14:13:52 +0200

    add create user function relates #39

--- src/containers/Firebase/firebase.js
+++ src/containers/Firebase/firebase.js
@@ -20,6 +20,9 @@ class Firebase {
     this.db = app.firestore();
   }
 
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
   user = uid => this.db.doc(`users/${uid}`);
 
   users = () => this.db.collection('users');
