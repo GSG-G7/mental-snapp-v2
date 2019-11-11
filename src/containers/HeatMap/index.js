88faff7a45e15fbd6e22d71ac675e078f1b30304
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-11-11 13:09:51 +0200

    used firebase to get and delete data

--- src/containers/HeatMap/index.js
+++ src/containers/HeatMap/index.js
@@ -1,38 +1,95 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { message } from 'antd';
 
 import HeatMap from './heatMap';
-import { heatMapData, journalsData } from './data';
+import { withFirebase } from '../Firebase';
+import filter from './filter';
 
 class index extends Component {
   state = {
-    data: [...heatMapData],
+    data: [],
     journals: [],
+    journalsOfTheDay: [],
   };
 
-  componentDidMount() {
-    // We need to get the heatMap data here
+  async componentDidMount() {
+    const { firebase, history } = this.props;
+    const firebaseData = [];
+    try {
+      const snapshot = await firebase.journals().get();
+      snapshot.forEach(doc => {
+        firebaseData.push({ id: doc.id, ...doc.data() });
+      });
+      const heatMapData = filter(firebaseData);
+      this.setState({ data: heatMapData, journals: firebaseData });
+    } catch (error) {
+      history.push('/server-error');
+    }
   }
 
   // This function should show data based on day
   handleClick = value => {
+    const { journals } = this.state;
     // the date of the clicked day in the same way it's stored in our DB
     if (value && value.count) {
       const currentDay = new Date(value.date).getDay();
-      const journalsOfTheDay = journalsData.filter(
+      const filteredJournals = journals.filter(
         journal => new Date(journal.timestamp).getDay() === currentDay
       );
-      this.setState({ journals: [...journalsOfTheDay] });
+      this.setState({ journalsOfTheDay: filteredJournals });
     } else {
-      this.setState({ journals: [] });
+      this.setState({ journalsOfTheDay: [] });
     }
   };
 
+  handleDelete = id => {
+    const { firebase } = this.props;
+    const { journals } = this.state;
+    // firebase
+    //   .journals()
+    //   .doc(id)
+    //   .delete()
+    //   .then(() => {
+    //     message.warning('This Journal is deleted');
+    //     this.setState({
+    //       journals: journals.filter(card => card.id !== id),
+    //       data: journals.filter(day => day.date !== id),
+    //     });
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+    //   });
+  };
+
+  handleJournalDetails = id => {
+    const {
+      history: { push },
+    } = this.props;
+    push(`/journal/${id}`);
+  };
+
   render() {
-    const { data, journals } = this.state;
+    const { data, journalsOfTheDay } = this.state;
     return (
-      <HeatMap data={data} handleClick={this.handleClick} journals={journals} />
+      <HeatMap
+        data={data}
+        handleClick={this.handleClick}
+        journals={journalsOfTheDay}
+        handleDelete={this.handleDelete}
+        handleJournalDetails={this.handleJournalDetails}
+      />
     );
   }
 }
 
-export default index;
+index.propTypes = {
+  firebase: PropTypes.shape({
+    journals: PropTypes.func.isRequired,
+  }).isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default withFirebase(index);
