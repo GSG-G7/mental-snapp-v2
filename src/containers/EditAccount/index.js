3b873851151d8a2bf4e4a8961cc8305cb8bb7ed2
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-11-11 14:16:17 +0200

    added the user info from the firebase function

--- src/containers/EditAccount/index.js
+++ src/containers/EditAccount/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import EditAccount from './editAccount';
 
+import { withFirebase } from '../Firebase';
+
 class Index extends Component {
   state = {
     info: {
@@ -9,20 +11,43 @@ class Index extends Component {
       email: 'fares@gmail.com',
     },
     checked: false,
+    error: '',
+  };
+
+  componentDidMount() {
+    const { firebase } = this.props;
+    const user = firebase.auth.currentUser;
+
+    if (user != null) {
+      const userInfo = {
+        name: user.displayName,
+        email: user.email,
+      };
+      this.setState({
+        info: userInfo,
+      });
+    }
+  }
+
+  handleErrorMessage = message => {
+    this.setState({ error: message });
   };
 
   onChange = ({ target: { checked } }) => this.setState({ checked });
 
   render() {
     const {
-      history: { goBack },
+      history: { goBack, push },
     } = this.props;
-    const { info, checked } = this.state;
+    const { info, checked, error } = this.state;
     return (
       <EditAccount
         userInfo={info}
         checked={checked}
         handleChange={this.onChange}
+        handlePush={push}
+        handleErrorMessage={this.handleErrorMessage}
+        error={error}
         handleGoBack={goBack}
       />
     );
@@ -32,7 +57,11 @@ class Index extends Component {
 Index.propTypes = {
   history: PropTypes.shape({
     goBack: PropTypes.func.isRequired,
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+  firebase: PropTypes.shape({
+    auth: PropTypes.object.isRequired,
   }).isRequired,
 };
 
-export default Index;
+export default withFirebase(Index);
