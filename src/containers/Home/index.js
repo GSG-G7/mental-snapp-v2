84be1a4d2055cf29849222de29b38a4d692733b2
AlaaTaima <alaa.ktaima@gmail.com>
2019-11-12 19:17:41 +0200

    render a page for every clicked journal that show it's details and also this journal can be deleted from it's page

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,40 +2,77 @@ import React, { Component } from 'react';
 import { message } from 'antd';
 import propTypes from 'prop-types';
 import HomePage from './home';
-import { journalsData, username, usergoal } from './staticData';
+import { withFirebase } from '../Firebase/index';
 
 class Home extends Component {
   state = {
     isEditable: false,
-    userName: 'Alaa Taima',
+    name: '',
     journals: [],
     goal: '',
+    recentJournals: [],
   };
 
   componentDidMount() {
-    // setState to journals and goal that we got from firebase
-    this.setState({
-      journals: journalsData,
-      userName: username,
-      goal: usergoal,
-    });
+    let { recentJournals } = this.state;
+    const { firebase } = this.props;
+    const userId = localStorage.getItem('userId');
+    firebase.db
+      .collection('users')
+      .doc(userId)
+      .onSnapshot(snapshot => {
+        const userGoal = snapshot.data().goal;
+        const userName = snapshot.data().name;
+        if (snapshot.data().userJournals) {
+          const userJournal = snapshot.data().userJournals;
+          if (userJournal.length === 0) {
+            return recentJournals;
+          }
+          if (userJournal.length > 3) {
+            recentJournals = userJournal.slice(-3);
+          } else if (userJournal.length <= 3) {
+            recentJournals = userJournal.slice(-1);
+          }
+          return this.setState({
+            journals: userJournal,
+            name: userName,
+            goal: userGoal,
+            recentJournals,
+          });
+        }
+        return this.setState({
+          name: userName,
+          goal: userGoal,
+        });
+      });
   }
 
   handleClick = () => this.setState({ isEditable: true });
 
   handelSave = () => {
+    const { goal } = this.state;
+    const { firebase } = this.props;
     this.setState({ isEditable: false });
-    // update the goal
+
+    const userId = firebase.auth.currentUser.uid;
+    firebase.user(userId).set({ goal }, { merge: true });
   };
 
   handleDelete = id => {
     const { journals } = this.state;
+    const { firebase } = this.props;
+    const userId = firebase.auth.currentUser.uid;
     message.warning('This Journal is deleted');
     // 1- this card will be deleted from firbase store.
+    firebase.db
+      .collection('users')
+      .doc(userId)
+      .update({
+        userJournals: journals.filter(journal => journal.timestamp !== id),
+      });
     // 2- also it will be deleted from state as follows :
-
     this.setState({
-      journals: journals.filter(card => card.id !== id),
+      journals: journals.filter(journal => journal.timestamp !== id),
     });
   };
 
@@ -44,17 +81,20 @@ class Home extends Component {
   handleJournalDetails = id => {
     const {
       history: { push },
+      match: { params },
     } = this.props;
+    params.id = id;
     push(`/journal/${id}`);
   };
 
   render() {
-    const { isEditable, userName, journals, goal } = this.state;
+    const { isEditable, name, journals, goal, recentJournals } = this.state;
     return (
       <HomePage
         isEditable={isEditable}
-        userName={userName}
+        userName={name}
         journals={journals}
+        recentJournals={recentJournals}
         goal={goal}
         handelSave={this.handelSave}
         handleClick={this.handleClick}
@@ -66,10 +106,20 @@ class Home extends Component {
   }
 }
 
-export default Home;
+export default withFirebase(Home);
 
 Home.propTypes = {
   history: propTypes.shape({
     push: propTypes.func.isRequired,
+  }).isRequired,
+  match: propTypes.shape({
+    params: propTypes.object.isRequired,
+  }).isRequired,
+  firebase: propTypes.shape({
+    auth: propTypes.object.isRequired,
+    uid: propTypes.string.isRequired,
+    user: propTypes.object.isRequired,
+    db: propTypes.object.isRequired,
+    collection: propTypes.object.isRequired,
   }).isRequired,
 };
