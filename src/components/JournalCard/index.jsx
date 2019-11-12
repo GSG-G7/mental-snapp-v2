7dd55659b3b9789a067a1c2fe29058ff5aac1848
AlaaTaima <alaa.ktaima@gmail.com>
2019-11-12 14:44:19 +0200

    render journal page Relates #112

--- src/components/JournalCard/index.jsx
+++ src/components/JournalCard/index.jsx
@@ -4,6 +4,10 @@ import PropTypes from 'prop-types';
 
 import './style.css';
 
+// convert function componet to class component
+
+// ComponenDidMount () {} to get stored data and render a page for clicked card
+
 const JournalCard = ({
   time,
   date,
