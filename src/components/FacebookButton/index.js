8d3d45385e693ecff0e525307e7446b3f1690fa7
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-11-12 12:20:52 +0200

    added functionality for the facebook button Relates #93

--- src/components/FacebookButton/index.js
+++ src/components/FacebookButton/index.js
@@ -1,41 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import { withFirebase } from '../../containers/Firebase';
 import { ReactComponent as FacebookImg } from '../../containers/assets/images/facebook.svg';
 import './style.css';
 
-const handleClick = () => {
-  // handle login or sign up with facebook
+const handleClick = async props => {
+  const { firebase } = props;
+  await firebase.auth.signInWithRedirect(firebase.facebookProvider);
 };
 
-class Facebook extends Component {
-  state = {};
-
-  async componentDidMount() {
-    const { firebase } = this.props;
-    const signedInUser = await firebase.auth.getRedirectResult();
-    if (signedInUser.credential) {
-      const token = signedInUser.credential.accessToken;
-      const { user } = signedInUser;
-      console.log(token, user);
-    }
-  }
-
-  handleClick = async () => {
-    const { firebase } = this.props;
-    await firebase.auth.signInWithRedirect(firebase.facebookProvider);
-  };
-
-  render() {
-    return (
-      <button type="submit" className="facebook-btn" onClick={handleClick}>
+const Facebook = props => {
+  return (
+    <>
+      <button
+        type="submit"
+        className="facebook-btn"
+        onClick={() => handleClick(props)}
+      >
         <FacebookImg className="facebook-btn__img" />
         <span className="facebook-btn__text">Facebook</span>
       </button>
-    );
-  }
-}
+    </>
+  );
+};
 
 Facebook.propTypes = {
   firebase: PropTypes.shape({
