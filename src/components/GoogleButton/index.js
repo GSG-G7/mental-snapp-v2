f01030f3b8f72aa91c75b42f447e150945e716a1
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-11-12 12:21:15 +0200

    added functionality for the google button Relates #93

--- src/components/GoogleButton/index.js
+++ src/components/GoogleButton/index.js
@@ -1,37 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import { withFirebase } from '../../containers/Firebase';
 import { ReactComponent as GoogleImg } from '../../containers/assets/images/google.svg';
 import './style.css';
 
-class GoogleButton extends Component {
-  state = {};
+const handleClick = async props => {
+  const { firebase } = props;
+  await firebase.auth.signInWithRedirect(firebase.googleProvider);
+};
 
-  async componentDidMount() {
-    const { firebase } = this.props;
-    const signedInUser = await firebase.auth.getRedirectResult();
-    if (signedInUser.credential) {
-      const token = signedInUser.credential.accessToken;
-      const { user } = signedInUser;
-      console.log(token, user);
-    }
-  }
-
-  handleClick = async () => {
-    const { firebase } = this.props;
-    await firebase.auth.signInWithRedirect(firebase.googleProvider);
-  };
-
-  render() {
-    return (
-      <button type="submit" className="google-btn" onClick={this.handleClick}>
-        <GoogleImg className="google-btn__img" />
-        <span className="google-btn__text">Google</span>
-      </button>
-    );
-  }
-}
+const GoogleButton = props => {
+  return (
+    <button
+      type="submit"
+      className="google-btn"
+      onClick={() => handleClick(props)}
+    >
+      <GoogleImg className="google-btn__img" />
+      <span className="google-btn__text">Google</span>
+    </button>
+  );
+};
 
 GoogleButton.propTypes = {
   firebase: PropTypes.shape({
