2d0e7786e348009e6593596517d78662c668b28a
alaa-yasin <alaayasin.ps@gmail.com>
2019-11-25 08:33:17 +0200

    all the emojies data

---
+++ src/components/JournalCard/EmojiData.js
@@ -0,0 +1,35 @@
+import happy from './images/happy.svg';
+import unhappy from './images/unhappy.svg';
+import inLove from './images/in-love.svg';
+import angry from './images/angry.svg';
+import surprised from './images/surprised.svg';
+
+const Feelings = [
+  {
+    id: 1,
+    photo: happy,
+    alt: 'happy emoji',
+  },
+  {
+    id: 2,
+    photo: unhappy,
+    alt: 'unhappy emoji',
+  },
+  {
+    id: 3,
+    photo: inLove,
+    alt: 'in love emoji',
+  },
+  {
+    id: 4,
+    photo: angry,
+    alt: 'angry emoji',
+  },
+  {
+    id: 5,
+    photo: surprised,
+    alt: 'surprised emoji',
+  },
+];
+
+export default Feelings;
