97afa0ee8a3f64ddb2ebd980091494ca526e976d
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-11-26 16:10:47 +0200

    fixed prop types error and refactored the jsx Relates #3

--- src/containers/Feed/feeds.jsx
+++ src/containers/Feed/feeds.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-nested-ternary */
 import React from 'react';
 import moment from 'moment';
 import PropTypes from 'prop-types';
@@ -9,18 +8,49 @@ import JournalCard from '../../components/JournalCard';
 import LogoHeader from '../../components/LogoHeader';
 import './feeds.css';
 
-// Getting the option from the select component
 const { Option } = Select;
 
 const Feed = props => {
   const {
-    currentMonthJournal,
+    currentMonthJournals,
     monthsWithCounts,
     loading,
     handleChange,
     handleDelete,
     handleJournalDetails,
   } = props;
+
+  const showCurrentMonthJournals = () => {
+    if (loading) {
+      return (
+        <div style={{ textAlign: 'center', marginTop: '9vh' }}>
+          <Spin size="large" />
+        </div>
+      );
+    }
+    if (currentMonthJournals.length > 0) {
+      return currentMonthJournals.map((journal, index) => (
+        <JournalCard
+          index={index}
+          key={journal.id}
+          time={moment(journal.timestamp).format('h:mm a')}
+          date={moment(journal.timestamp).format('MMMM Do YYYY')}
+          grateful={journal.grateful && journal.grateful.title}
+          challenge={journal.challenge && journal.challenge.title}
+          developing={journal.developing && journal.developing.title}
+          handleDelete={() => handleDelete(journal.id)}
+          journalId={journal.id}
+          handleJournalDetails={handleJournalDetails}
+        />
+      ));
+    }
+    return (
+      <h2 className="feeds__message">
+        No entries for this month, choose another one
+      </h2>
+    );
+  };
+
   return (
     <div className="feeds">
       <div className="feeds__content container">
@@ -31,43 +61,16 @@ const Feed = props => {
           className="feeds__select"
           onChange={handleChange}
         >
-          {monthsWithCounts.map(element => (
-            <Option
-              className="feeds__option"
-              key={element.id}
-              value={element.month}
-            >
-              <span>{element.month}</span>
-              <span>{element.count}</span>
+          {monthsWithCounts.map(({ id, month, count }) => (
+            <Option className="feeds__option" key={id} value={month}>
+              <span>{month}</span>
+              <span>{count}</span>
             </Option>
           ))}
         </Select>
       </div>
       <div className="feed_journals container">
-        {/* we need to implement a readable function here */}
-        {loading ? (
-          <div style={{ textAlign: 'center', marginTop: '9vh' }}>
-            <Spin size="large" />
-          </div>
-        ) : currentMonthJournal.length > 0 ? (
-          currentMonthJournal.map(journal => (
-            <JournalCard
-              key={journal.id}
-              time={moment(journal.timestamp).format('h:mm a')}
-              date={moment(journal.timestamp).format('MMMM Do YYYY')}
-              grateful={journal.grateful && journal.grateful.title}
-              challenge={journal.challenge && journal.challenge.title}
-              developing={journal.developing && journal.developing.title}
-              handleDelete={() => handleDelete(journal.id)}
-              journalId={journal.id}
-              handleJournalDetails={handleJournalDetails}
-            />
-          ))
-        ) : (
-          <h2 className="feeds__message">
-            No entries for this month, choose another one
-          </h2>
-        )}
+        {showCurrentMonthJournals()}
       </div>
       <NavBar />
     </div>
@@ -75,8 +78,8 @@ const Feed = props => {
 };
 
 Feed.propTypes = {
-  currentMonthJournal: PropTypes.arrayOf.isRequired,
-  monthsWithCounts: PropTypes.arrayOf.isRequired,
+  currentMonthJournals: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+  monthsWithCounts: PropTypes.arrayOf(PropTypes.shape()).isRequired,
   loading: PropTypes.bool.isRequired,
   handleChange: PropTypes.func.isRequired,
   handleDelete: PropTypes.func.isRequired,
